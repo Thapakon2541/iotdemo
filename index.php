c37c4ac87ff1912a092d2f2bf10d70a2200f557c
peenthapao <36725707+peenthapao@users.noreply.github.com>
2019-09-16 15:24:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!-- <!-- <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -50,16 +50,67 @@
     <button id="btn">
         click
     </button>
+    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
   <script>
-      $("#btn").click(function(){
-        alert("test1")
+  function plot (dataPoints,dataPoints2) {
+      
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            theme: "light2",
+            title:{
+                text: "Simple Line Chart"
+            },
+            axisY:{
+                includeZero: false
+            },
+        
+            data: [{        
+                type: "line",       
+                dataPoints:dataPoints,
+                  
+            },
+            {        
+                type: "line",       
+                dataPoints:dataPoints2
+            }
+
+        
+        ] 
         });
-  
+        
+        chart.render();
+        }
+    // //   function btn(){
+    // //       alert("test");
+    // //   }
+    // //   $("#btn").click(function(){
+    // //     alert("test1")
+    // //     });
+    $(()=>{
+        $("button").click(function(){
+            let url = "https://api.thingspeak.com/channels/860698/feeds.json?results=50";
+            $.get(url,function(data,status) {
+                console.log(JSON.stringify(data));
+               var dataPoints=[];
+               var dataPoints2=[]
+                $.each(data.feeds,function(key,val) {
+                    console.log(data.feeds)
+                    dataPoints.push({x:val.entry_id,y:parseInt(val.field1)});
+                    dataPoints2.push({x:val.entry_id,y:parseInt(val.field2)});
+                })
+                
+                plot(dataPoints,dataPoints2)
+            })
+            
+        });
+    });
+    
   </script>
-</html>
+        </html>
